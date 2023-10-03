e23850df5f1c0fe3c461005857ae2bdfeea9f616
Zoren Villareal <zoren.villareal@gmail.com>
2023-10-03 16:47:51 -0700

    finished

---
+++ functions_practice.py
@@ -0,0 +1,22 @@
+def hello(user_name):
+  print(f"Hi, {user_name}. How are you today")
+
+def pack(arg1, arg2, arg3):
+  return [arg1,arg2,arg3]
+
+def eat_lunch(my_lst):
+  if len(my_lst) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(my_lst)):
+      if i == 0:
+        print(f"First I eat {my_lst[0]}")
+      else:
+        print(f"Next I eat {my_lst[i]}")
+
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
